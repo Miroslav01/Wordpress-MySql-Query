a743a447b7ff0f48cdef9dd476082806a9e0e665
Mike Stratton <mike@mikestratton.net>
2013-08-15 05:39:44 -0400

    Create wp-posts.php
    Displays Wordpress posts, in raw data format, pulling from the MySql / wp-posts table.

---
+++ wp-posts.php
@@ -0,0 +1,31 @@
+// Display all Posts from Wordpress
+<?php
+
+//MySQL Database Connect
+include 'dataconnect.php';
+
+// Pull from tables
+$usertable="wp_posts";
+$yourfield = "post_title";
+
+mysql_connect($hostname,$username, $password) or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.'),history.go(-1)</script></html>");
+mysql_select_db($dbname);
+
+# Check If Record Exists
+
+$query = "SELECT * FROM wp_posts";
+
+$result = mysql_query($query);
+
+if($result)
+{
+while($row = mysql_fetch_array($result))
+{
+$name = $row["post_content"];
+echo "Name: ".$name."<br><br>";
+}
+}
+
+
+
+?>
